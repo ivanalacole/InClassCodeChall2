2ac4604afd919f925d27fa3553473b9a88c15ae5
Ivana Burton Thompson <IvanaBT@R2Hs-MBP.attlocal.net>
2022-04-15 13:54:33 -0400

    pushing code challenge

---
+++ challenge.js
@@ -0,0 +1,15 @@
+//Question 1
+let balance = 100
+const initialPrompt = prompt('How may I help you today ?')
+
+if (initialPrompt === 'Deposit Money') {
+    prompt('How Much would you like to deposit?');
+} else if(initialPrompt === 'Withdraw Money') {
+    prompt('How much would you like to withdraw ?')
+}
+
+if(deposit === 'Deposit Money') {
+    prompt('How much would you like to deposit')
+} else if(withdraw == 'Withdraw Money') {
+    prompt('How much would you like to withdraw')
+}
